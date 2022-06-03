8211afd0caf8ee04c26b36bdcb08b73f6095164b
Citrusbug <tushar.citrusbug@gmail.com>
2022-06-03 15:39:14 +0530

    added pagination

--- new project/stepform.js
+++ new project/stepform.js
@@ -1,32 +1,58 @@
 
 
 var current_page=1;
+
+$(document).ready(function () {
+     
+    changePage(1);
+ShowDots(current_page-1);
+
+  });
+
 $('#nextBtn').on('click',function () {
     nextPage();
+    // ShowDots();
 })
 
 $('#prevBtn').on('click',function () {
     prevPage();
+    // ShowDots();
 })
 var form=$('#regForm').children();
+
+
 var dots=$('#steps').children();
 
-Nextbtn(counter);
-ShowDots(counter);
+Nextbtn(current_page);
+
 function prevPage() {
-     
+  console.log(current_page,"prev");
+  ShowDots(current_page-1);
+
+    document.getElementById('submit').hidden = true;
+    
+    document.getElementById('nextBtn').hidden = false;
+    if (current_page===1) {
+        document.getElementById('prevBtn').hidden = true;
+    }
     if (current_page > 1) {
       current_page--;
       changePage(current_page);
     }
   }
 function nextPage() {
-     
-    if (current_page===numPages()-1) {
-    //   btn_next.style.visibility = "hidden";
+ShowDots(current_page);
+
+    document.getElementById('prevBtn').hidden = false;
+  console.log(current_page,"next");
+    if (current_page===3) {
+      document.getElementById('nextBtn').hidden = true;
+      document.getElementById('prevBtn').hidden = false;
+      document.getElementById('submit').hidden = false;
+
       
     }
-    if (current_page < numPages()) {
+    if (current_page < 4) {
       current_page++;
       changePage(current_page);
     }
@@ -39,6 +65,8 @@ function ShowDots(num) {
     for (let index = 0; index < dots.length; index++) {
         if (index===num) {
             dots[index].classList.add('step-fill');
+         }else{
+            dots[index].classList.remove('step-fill');
          }
     }
 }
@@ -55,5 +83,7 @@ function Nextbtn(num) {
 
 
 function changePage(current_page) {
- console.log(current_page);   
+ if (current_page==1) {
+    document.getElementById('prevBtn').hidden = true; 
+ }
 }
